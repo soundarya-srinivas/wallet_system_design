3cdd564d615761a141cf05091f05b6b321c7ade8
Soundarya S <soundaryasrinivas8299@gmail.com>
2022-10-11 22:56:12 +0530

    updating db in server

--- server/DbConfig/db.js
+++ server/DbConfig/db.js
@@ -3,7 +3,7 @@ const {MONGO_URI}=require("../config/valuekeys");
 
 const connectDB = () => {
     
-  mongoose.connect(process.env.MONGO_URI, {
+  mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
